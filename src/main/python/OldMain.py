2bfce61ea6724b542dd40e054e795341251a73bc
Oren Anderson <42502680+Raviolifun@users.noreply.github.com>
2021-07-28 23:54:33 -0400

    added installer and reorginized

---
+++ src/main/python/OldMain.py
@@ -0,0 +1,17 @@
+# Internal imports
+
+# External imports
+
+# Self imports
+from src.main.python import Sub_Functions as Sub_Fun
+
+Sub_Fun.download()
+
+value = "Suck my dick"
+for i in range(10):
+    value = Sub_Fun.synoantonym_string(value, "a an the i it as its no in", True)
+    print(value)
+
+
+
+# Sub_Fun.synoantonym_file("BeeMovie.txt", "a an the i it as its no in", True)
